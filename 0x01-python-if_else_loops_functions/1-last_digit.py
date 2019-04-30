531b1686e3500d75104554a593802edc2ff7c551
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-04-30 21:58:00 +0000

    code init

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+strnum = str(number)
+lastdigit = int(strnum[-1])
+if lastdigit > 5:
+    stri = "and is greater than 5"
+elif lastdigit == 0:
+    stri = "and is 0"
+else:
+    stri = "and is less than 6 and not 0"
+
+print("Last digit of {0} is {1} {2}".format(number, lastdigit, stri))
