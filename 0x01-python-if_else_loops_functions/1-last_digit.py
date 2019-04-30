c5f7cbca733968df994157a886611655f74e6660
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-04-30 22:12:38 +0000

    correct signed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+signed = ""
+if number < 0:
+    signed = "-"
 strnum = str(number)
 lastdigit = int(strnum[-1])
 if lastdigit > 5:
@@ -10,4 +13,4 @@ elif lastdigit == 0:
 else:
     stri = "and is less than 6 and not 0"
 
-print("Last digit of {0} is {1} {2}".format(number, lastdigit, stri))
+print("Last digit of {0} is {1}{2} {3}".format(number, signed, lastdigit, stri))
