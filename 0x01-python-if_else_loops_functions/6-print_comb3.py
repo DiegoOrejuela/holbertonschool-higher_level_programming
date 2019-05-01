b3f8de60441fc130e4d2909ecdd6a3a0c5430760
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-01 00:21:14 +0000

    correction space

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(8):
-    for j in range (1, 10):
+    for j in range(1, 10):
         if i != j and i < j:
             print("{0}{1}, ".format(i, j), end="")
 print("{}".format(89))
