318f43503febf3e9822184cc4e18afcca60b6393
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-01 22:02:00 +0000

    delete whitespaces

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from sys import argv
     len_argv = len(argv) - 1
     print("{} argument{}".format(len_argv, "s" if len_argv != 1 else ""))
-    for i in range (1, len(argv)):
+    for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
