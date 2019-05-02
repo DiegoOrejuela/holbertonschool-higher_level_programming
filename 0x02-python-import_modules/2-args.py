a899cb87b0cfcdb00cb37bbafd6106d0f7f53f50
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-02 15:23:37 +0000

    correction doble linea continue code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
     len_argv = len(argv) - 1
-    print("{} argument{}{}".format(len_argv, "s" if len_argv != 1 else "",
-                                 "." if len_argv == 0 else ":"))
+    point = ":"
+    plural = "s"
+    if len_argv == 0:
+        point = "."
+    if len_argv == 1:
+        plural = ""
+    print("{} argument{}{}".format(len_argv, plural, point))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
