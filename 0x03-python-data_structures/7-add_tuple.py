b30bae15f21ebb148b7ab075281369d026cfd326
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-08 03:45:09 +0000

    condition when the arguents is > 1

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,13 +5,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     if len(tuple_a) == 1:
         a0 = tuple_a[0]
-    elif len(tuple_a) == 2:
+    elif len(tuple_a) > 1:
         a0 = tuple_a[0]
         a1 = tuple_a[1]
 
     if len(tuple_b) == 1:
         b0 = tuple_b[0]
-    elif len(tuple_b) == 2:
+    elif len(tuple_b) > 1:
         b0 = tuple_b[0]
         b1 = tuple_b[1]
 
