b83aaf25869e08cefcfb9ab0c2a51e2dc828ba32
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-10 05:14:08 +0000

    case is not str

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not str(roman_string).isalpha():
+        return(0)
     if not roman_string:
         return None
     num_rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
