117eb88affbc5aac2467a3eee3da4201b73f5d9a
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-14 03:35:35 +0000

    code init

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,55 @@
+#include <Python.h>
+#include <stdio.h>
+#include <bytesobject.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p: object type list.
+ *
+ * Return: nothing.
+ */
+void print_python_list(PyObject *p)
+{
+	PyListObject *list = (PyListObject *)p;
+	Py_ssize_t size_list = PyList_Size(p);
+	Py_ssize_t i;
+
+        printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %lu\n", size_list);
+	printf("[*] Allocated = %lu\n", (*list).allocated);
+
+	for (i = 0; i < size_list; i++)
+	{
+		printf("Element %lu: %s\n", i, (*Py_TYPE((*list).ob_item[i])).tp_name);
+	}
+}
+
+/**
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p: object type list.
+ *
+ * Return: nothing.
+ */
+void print_python_bytes(PyObject *p)
+{
+       	char *str;
+	int i;
+	PyBytesObject *bstring;
+
+	bstring = (PyBytesObject *)p;
+	if (bstring == NULL)
+		printf("aquí estamos");
+
+	printf("[.] bytes object info\n");
+	printf("  size: %lu\n", PyBytes_Size(p));
+
+	str = PyBytes_AsString(p);
+	printf("  trying string: %s\n", str);
+
+	printf("  first %lu bytes:", strlen(str));
+	for (i = 0; str[i] != '\0' && i < 10; i++)
+	{
+		printf(" %02x", str[i]);
+	}
+	printf("\n");
+}
