18cfdc83f7ec1edd6b41cd3ec1bf2f2e21221bb7
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-20 16:58:17 +0000

    correction return number

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/pyhton3
 def safe_print_list(my_list=[], x=0):
+    counter = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
+            counter += 1
         print()
-        return i + 1
+        return counter
     except IndexError:
         print()
-        return(5)
+        return(counter)
