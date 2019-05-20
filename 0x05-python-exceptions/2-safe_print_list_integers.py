c25083ff0d179e2dff33b7f087516e1b06005924
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-20 17:07:52 +0000

    executable file

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+        print()
+        return i + 1
+    except ValueError:
+        return(10)
