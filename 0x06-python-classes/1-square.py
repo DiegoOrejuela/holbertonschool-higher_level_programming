8c7a17e128b1b9831fdc5f2a996a550f3ac7f73d
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-21 20:44:13 +0000

    code init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ Create to type of square"""
+    def __init__(self, size):
+        """Example of docstring on the __init__ method.
+
+        args:
+            size: num of size.
+        """
+        self.__size = size
