f9496fc6d36977ba028433fcb2a661d091d3ef2f
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-24 16:39:42 +0000

    code init

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module 4-print_square.py | Test-driven development Package
+
+What you should learn from this project:
+- What’s an interactive test and why tests are important
+- How to write Docstrings to create tests and he basic option flags"""
+
+
+def print_square(size):
+    """4-print_square - prints a square with the character #.
+    Arguments: size is the size length of the square.
+    Return:Nothing"""
+
+    if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+    else:
+        raise TypeError("size must be an integer")
+
+    if size == 0:
+        print()
+    else:
+        for i in range(size):
+            print('#' * size)
