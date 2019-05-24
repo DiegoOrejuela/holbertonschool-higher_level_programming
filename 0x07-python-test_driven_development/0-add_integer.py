dccd0cd5464bf80307ae13daf2c224d663b34b2f
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-24 14:36:25 +0000

    correction docstrings and pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Module 0-add_integer-py | Test-driven development Package
+
+What you should learn from this project:
+- What’s an interactive test and why tests are important
+- How to write Docstrings to create tests and he basic option flags"""
+
 
 def add_integer(a, b=98):
+    """add_integer - adds 2 integers.
+    Arguments: a and b must be integers or floats
+    Return:the addition of a and b"""
+
     n = [a, b]
     n_str = ['a', 'b']
     for i in range(2):
