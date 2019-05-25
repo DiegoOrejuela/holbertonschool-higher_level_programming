3a7657c0d2dae60ca2065f480af6e06d533606c9
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-25 04:45:53 +0000

    correction style

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,27 +11,26 @@ def matrix_divided(matrix, div):
     Arguments: matrix must be a list of lists of integers or floats.
     Return: new matrix with value divided"""
 
-    # Review div
-    if type(div) == float and type(div) != int:
-            n[i] = int(n[i])
-    elif type(div) != int:
-        raise TypeError("div must be a number")
-    elif div == 0:
-        raise ZeroDivisionError("division by zero")
+    # Review if number are Int/float and convert int in float
+    str_err = "matrix must be a matrix (list of lists) of integers/floats"
+    for i in range(len(matrix)):
+        if type(matrix[i]) != list:
+            raise TypeError(str_err)
+        for j in range(len(matrix[i])):
+            if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
+                raise TypeError(str_err)
 
     # Review Len List
     for row in matrix:
         if len(matrix[0]) != len(row):
             raise TypeError("Each row of the matrix must have the same size")
 
-    # Review if number are Int/float and convert int in float
-    str_err = "matrix must be a matrix (list of lists) of integers/floats"
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if type(matrix[i][j]) == float and type(matrix[i][j]) != int:
-                matrix[i][j] = int(matrix[i][j])
-            elif type(matrix[i][j]) != int:
-                raise TypeError(str_err)
+    # Review div
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
+
     # Div all elements of matrix
     new_matrix = []
     for i in range(len(matrix)):
