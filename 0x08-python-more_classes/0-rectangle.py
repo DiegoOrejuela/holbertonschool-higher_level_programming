3810c66e6fc46124338035bc4267f8e069ffce82
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-27 19:49:14 +0000

    code init

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 0-rectangle | More Classes and Objects
+
+What you should learn from this project:
+- What is a class, What is an object and an instance
+- What is the difference between a class and an object or instance"""
+
+
+class Rectangle:
+    """Rectangle - defines a rectangle.
+    Attributes: nothing.
+    Method: nothing."""
+
+    pass
