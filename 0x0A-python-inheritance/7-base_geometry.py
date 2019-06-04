7198b562948d02c421978ca61ced5be07462b9ef
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-06-04 23:50:45 +0000

    docustring

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
+"""7-base_geometry | Inheritance
+    What you should learn from this project:
+    - What’s an interactive test and why tests are important
+    - How to write Docstrings to create tests and he basic option flags"""
 
 
 class BaseGeometry:
+    """Base Geometry
+    Methods:
+    - area
+    - integer_validator
+    Arguments = no one"""
+
     def area(self):
+        """ area - that raises an Exception with the message area() is not
+        implemented.
+        Arguments: none.
+        Return: nothing"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """ integer_validor - validates value.
+        Arguments: name and value.
+        Return: nothing"""
         if type(value) == int:
             if value <= 0:
                 raise ValueError(name + " must be greater than 0")
