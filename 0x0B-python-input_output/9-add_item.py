0bcb20d59e3f918a8e7a4e122a62994a11495a31
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-06-05 13:02:22 +0000

    code init

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from sys import argv
+from pathlib import Path
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+list_result = []
+
+file_ok = Path("add_item.json")
+if file_ok.is_file():
+    str_py = load_from_json_file("add_item.json")
+    list_result += str_py
+
+list_argv = argv
+del list_argv[0]
+list_result += list_argv
+
+save_to_json_file(list_result, "add_item.json")
