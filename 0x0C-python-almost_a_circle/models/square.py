24736f1400d30f6cd66ed62bb3ef87421a7567b1
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-06-08 20:49:27 +0000

    T11: Update the class Square by adding the public getter and setter size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,9 +7,35 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-
+    """Square - defines a square.
+    Attributes: inheritance by Rectangle.
+    Methods:__init__, [width, height, x, y] setter and getter.
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """__init__ - method constructor.
+        Args:
+            size (int): width of rectangle.
+            x (int): position x
+            y (int):  position y
+            id (int): id square
+        """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """size - get size.
+        Args: nothing
+        """
+        return super().width
+
+    @size.setter
+    def size(self, value):
+        """size - set size.
+        Args:
+        - value (int): set width and height of rectangle.
+        """
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """__str__ - Create a new string object from the given object.
