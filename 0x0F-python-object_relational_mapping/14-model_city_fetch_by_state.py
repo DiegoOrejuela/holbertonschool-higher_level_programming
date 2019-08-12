5b7866723a30d02be65c534a6ea34aff47ca88bb
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-08-12 00:37:33 +0000

    T14: prints all City objects from the database hbtn_0e_14_usa

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+14-model_state_delete.py | prints all City objects from the database
+hbtn_0e_14_usa.
+"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    records = session.query(State.name, City.id, City.name)\
+                     .select_from(State)\
+                     .join(City)\
+                     .order_by(State.id)\
+                     .all()
+
+    for row in records:
+        print("{}: ({}) {}".format(row[0], row[1], row[2]))
+    session.close()
