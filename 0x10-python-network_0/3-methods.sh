7d529d988bfb68bef8139db2f46c3827e9848e70
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-08-20 22:08:03 +0000

    fix lengt 4

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL and displays all HTTP methods the server will accept
-sudo curl -sD - $1 -o /dev/null | grep Allow | cut -d " " -f 2-
-  
+sudo curl -sD - $1 -o /dev/null | grep Allow | cut -d " " -f 2- 
