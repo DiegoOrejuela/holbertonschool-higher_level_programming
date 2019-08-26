f29fae170be16afd9dbce399a2e5ed36b3e2ff6d
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-08-26 18:38:57 +0000

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module - hbtn_status"""
+
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("""Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(type(html), html, html.decode()))
