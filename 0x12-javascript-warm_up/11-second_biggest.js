1d253ef3238f012e4160465de5f823a5b6cfb88b
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-09-17 22:36:49 +0000

    [add] T11: script that searches the second biggest integer in the list of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const argv = require('process').argv;
+
+if (isNaN(parseInt(argv[2])) || parseInt(argv[2]) === 1) {
+  console.log(0);
+} else {
+  const myList = [];
+  for (let i = 2; i < argv.length; i++) {
+    myList.push(parseInt(argv[i]));
+  }
+  myList.sort(function (a, b) { return b - a; });
+  console.log(myList[1]);
+}
