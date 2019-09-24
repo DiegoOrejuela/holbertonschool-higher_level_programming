ede8c93053f82abbd4adfc80ef84a768d89815dc
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-09-24 05:04:33 +0000

    T10: whitout new variable

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  const b = base;
 
   function mybase (n) {
-    return (n.toString(b));
+    return (n.toString(base));
   }
   return (mybase);
 };
