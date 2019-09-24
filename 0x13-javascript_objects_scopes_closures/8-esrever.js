ebf8db135010fa7acd541a2f865ca10cfa06d3d7
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-09-24 04:22:20 +0000

    T8 [feature]: whitout option reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return (list.reverse());
+  const myList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    myList.push(list[i]);
+  }
+  return (myList);
 };
